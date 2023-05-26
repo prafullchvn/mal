76b6198afbd965af0d33d5f7f5dc1670f523dcf5
Prafull Chavan <99485204+prafullchvn@users.noreply.github.com>
2023-05-26 11:00:22 +0530

    Refactor setp 0.

--- impls/prafull/step0_repl.js
+++ impls/prafull/step0_repl.js
@@ -1,17 +1,32 @@
-const { stdin, stdout } = process;
+// const { stdin, stdout } = process;
 
 const READ = (arg) => arg;
 const EVAL = (arg) => arg;
 const PRINT = (arg) => arg;
 const rep = (arg) => PRINT(EVAL(READ(arg)));
 
-const main = () => {
-  stdin.setEncoding('utf8');
-  stdout.write('user> ');
-  stdin.on('data', (line) => {
-    stdout.write(rep(line));
-    stdout.write('user> ');
-  });
-};
+// const main = () => {
+//   stdin.setEncoding('utf8');
+//   stdout.write('user> ');
+//   stdin.on('data', (line) => {
+//     stdout.write(rep(line));
+//     stdout.write('user> ');
+//   });
+// };
 
-main();
+// main();
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const repl = () =>
+  rl.question('user> ', (line) => {
+    console.log(line);
+    repl();
+  });
+
+repl();
